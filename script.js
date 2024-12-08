98a1133b2688134978a121505f0f12b3c7331dd2
Abhishek yadav <timepass80100@gmail.com>
2024-12-08 20:59:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let music = document.querySelector('#dd');
 let icon = document.getElementById('vol-icon')
 let vol = document.getElementById('vol')
 let currentTimeDisplay = document.getElementById('current-time');
+let h = document.getElementById('h');
 
 
 play.addEventListener('click', () => {
@@ -22,18 +23,52 @@ play.addEventListener('click', () => {
 
 });
 
-
 vol.addEventListener('input', (event) => {
 
     let volume = event.target.value
     music.volume = volume;
 
-    if (volume == 0) {icon.className = "fa fa-volume-mute";}
+    if (volume == 0) {icon.className = "fa fa-volume-mute";
 
-    else if (volume <= 0.5) {icon.className = "fa fa-volume-down"; }
+ 
+    let aa = h.textContent;
 
-    else {icon.className = "fa fa-volume-up";}
-      icon.style.opacity = vol.value > 0 ? 1 : 0.5;
+
+h.textContent = "V-Mute"; 
+
+
+setTimeout(() => {
+    h.textContent = "Music"; 
+
+}, 2000);
+ 
+    }
+   
+
+    else if (volume <= 0.5) {icon.className = "fa fa-volume-down"; 
+       
+        let bb = h.textContent;
+        h.textContent = "V-normal";
+
+        setTimeout((volume) => {
+            h.textContent = 'Music';
+        },2000);
+       
+    
+
+    }
+
+    else {icon.className = "fa fa-volume-up";
+    let cc = h.textContent;
+        h.textContent = "V-Max";
+
+        setTimeout(() => {
+            h.textContent = 'Music';
+        },2000);
+
+    }
+    
+       icon.style.opacity = vol.value > 0 ? 1 : 0.5;
 
 });
 
